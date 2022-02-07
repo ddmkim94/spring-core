38ce8158cb260ed2363f79c2eafd7c752c460e42
dmkim <ddmkim94@gmail.com>
2022-02-07 15:43:21 +0900

    스프링으로 전환! 스프링 컨테이너로 스프링 빈 가져오기

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -4,12 +4,19 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
 
-        AppConfig config = new AppConfig();
-        MemberService memberService = config.memberService();
+        /*
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberService();
+        */
+
+        ApplicationContext ctx = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = ctx.getBean("memberService", MemberService.class);
         Member member = new Member(1L, "은빈", Grade.VIP);
 
         memberService.join(member);
