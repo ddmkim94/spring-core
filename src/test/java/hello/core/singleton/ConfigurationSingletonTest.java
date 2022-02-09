892c81acec363173d880aecaaa63a12b35a4cb51
dmkim <ddmkim94@gmail.com>
2022-02-09 17:38:33 +0900

    @Configuration이 싱글톤을 보장해주는 이유 바이트코드를 조작하는 CGLIB

--- src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
+++ src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
@@ -2,11 +2,8 @@ package hello.core.singleton;
 
 import hello.core.AppConfig;
 import hello.core.member.MemberRepository;
-import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
-import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -31,5 +28,14 @@ public class ConfigurationSingletonTest {
 
         assertThat(memberService.getMemberRepository()).isSameAs(memberRepository);
         assertThat(orderService.getMemberRepository()).isSameAs(memberRepository);
+    }
+
+    @Test
+    void configurationDeep() {
+        ApplicationContext ctx = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        AppConfig appConfig = ctx.getBean(AppConfig.class);
+
+        System.out.println("appConfig = " + appConfig.getClass());
     }
 }
