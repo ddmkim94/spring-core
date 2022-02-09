b753ccba82a21085d10dcd4f6eaf5295e9acbc80
dmkim <ddmkim94@gmail.com>
2022-02-09 16:04:36 +0900

    @Configuration과 싱글톤 AppConfig 싱글톤 테스트

---
+++ src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
@@ -0,0 +1,35 @@
+package hello.core.singleton;
+
+import hello.core.AppConfig;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class ConfigurationSingletonTest {
+
+    @Test
+    void configurationTest() {
+
+        ApplicationContext ctx = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        // 구체 타입으로 조회한 이유? -> 구현체 클래스 안의 getMemberRepository() 호출을 위해!!
+        MemberServiceImpl memberService = ctx.getBean("memberService", MemberServiceImpl.class);
+        OrderServiceImpl orderService = ctx.getBean("orderService", OrderServiceImpl.class);
+        MemberRepository memberRepository = ctx.getBean("memberRepository", MemberRepository.class);
+
+        System.out.println("memberService = " + memberService.getMemberRepository());
+        System.out.println("orderService = " + orderService.getMemberRepository());
+        System.out.println("memberRepository = " + memberRepository);
+
+        assertThat(memberService.getMemberRepository()).isSameAs(memberRepository);
+        assertThat(orderService.getMemberRepository()).isSameAs(memberRepository);
+    }
+}
