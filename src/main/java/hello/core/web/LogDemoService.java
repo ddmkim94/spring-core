ce2929c3090d4ed63758731134288db4caa58549
ddmkim <ddmkim94@gmail.com>
2022-02-20 15:46:32 +0900

    request 스코프 예제 Proxy 방식으로 개선 (강의 END)

--- src/main/java/hello/core/web/LogDemoService.java
+++ src/main/java/hello/core/web/LogDemoService.java
@@ -2,18 +2,15 @@ package hello.core.web;
 
 import hello.core.common.MyLogger;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class LogDemoService {
 
-    // private final MyLogger myLogger;
-    private final ObjectProvider<MyLogger> provider;
+    private final MyLogger myLogger;
 
     public void logic(String id) {
-        MyLogger myLogger = provider.getObject();
         myLogger.log("service id = " + id);
     }
 }
